7bd6c1b3d955b4f6e229f9edd81c3adc4ede281b
Steven Giesel <stgiesel35@gmail.com>
2021-04-03 11:36:29 +0200

    Expanded rope with concat

--- index.ts
+++ index.ts
@@ -3,4 +3,5 @@ export { getLongestCommonSubsequence } from './src/edit-distance/longest-common-
 export { getLevenshteinDistance } from './src/edit-distance/levenshtein'
 export { getHammingDistance } from './src/edit-distance/hamming-distance'
 export { Trie } from './src/data-structure/trie'
+export { Rope } from './src/data-structure/rope'
 export { contains, findAll } from './src/search/boyer-moore'
--- src/data-structure/rope.ts
+++ src/data-structure/rope.ts
@@ -1,3 +1,6 @@
+/**
+ * Represents a rope
+ */
 export class Rope {
     private fragment: string
     private hasToRecaluclateWeights: boolean
@@ -13,6 +16,11 @@ export class Rope {
       this.weight = 0
     }
 
+    /**
+     * Returns the character at the given index
+     * @param index The zero-based index of the desired character.
+     * @returns Returns the character at the specified index.
+     */
     public charAt (index: number): string {
       if (this.hasToRecaluclateWeights) {
         this.CalculateAndSetWeight()
@@ -21,12 +29,68 @@ export class Rope {
       return Rope.charAtInternal(this, index)
     }
 
+    /**
+     * Represents the rope as a single string
+     * @returns The whole rope as string
+     */
     public toString (): string {
       const results: string[] = []
       Rope.getStrings(this, results)
       return results.join('')
     }
 
+    /**
+     * Concats a string to the rope and returns the new rope
+     * @param other Other string which will be appended to the rope
+     * @param recalculateWeights If set to true the weights of the new rope will be calculated immediately
+     * @returns New rope instance with both texts
+     * @remarks If concating a lot of strings, setting recalculateWeights to true is very expensive.
+     * Operations which require the calculated weight will check if a recalculation is needed.
+     * ```ts
+     * for (let i = 0; i < 10000; i++) {
+     *   newRope = newRope.concat('test', false)
+     * }
+     * newRope.charAt(2) // This will automatically recalculate the weight
+     * ```
+     */
+    public concatString (other: string, recalculateWeights = false): Rope {
+      const otherRope = Rope.Create(other)
+      return this.concatRope(otherRope, recalculateWeights)
+    }
+
+    /**
+     * Concats a rope to the current one and returns the new combined rope
+     * @param other Other string which will be appended to the rope
+     * @param recalculateWeights If set to true the weights of the new rope will be calculated immediately
+     * @returns New rope instance with both texts
+     * @remarks If concating a lot of strings, setting recalculateWeights to true is very expensive.
+     * Operations which require the calculated weight will check if a recalculation is needed.
+     * ```ts
+     * for (let i = 0; i < 10000; i++) {
+     *   newRope = newRope.concat('test', false)
+     * }
+     * newRope.charAt(2) // This will automatically recalculate the weight
+     * ```
+     */
+    public concatRope (other: Rope, recalculateWeights = false): Rope {
+      const newRope = new Rope()
+      newRope.left = this
+      newRope.right = other
+      newRope.hasToRecaluclateWeights = true
+
+      if (recalculateWeights) {
+        newRope.CalculateAndSetWeight()
+      }
+
+      return newRope
+    }
+
+    /**
+     * Creates the rope with the given text
+     * @param text The initial text to add in the rope
+     * @param leafLength Size of a single leaf. Every leaf is a substring of the given text
+     * @returns Instance of a rope
+     */
     public static Create (text: string, leafLength = 8): Rope {
       return this.CreateInternal(text, leafLength, 0, text.length - 1)
     }
